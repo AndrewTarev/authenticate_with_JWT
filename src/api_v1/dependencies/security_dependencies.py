88ff42b47559f6505166c7a837789a0acd377525
AndrewTarev <153756623+AndrewTarev@users.noreply.github.com>
2024-09-09 21:15:27 +0800

    delete comments

--- src/api_v1/dependencies/security_dependencies.py
+++ src/api_v1/dependencies/security_dependencies.py
@@ -109,39 +109,3 @@ def get_current_active_auth_user(
     """
     if user.active:
         return user
-
-
-# async def get_current_user(
-#     token: Annotated[str, Depends(oauth2_scheme)],
-#     session: Annotated[AsyncSession, Depends(db_helper.get_db)],
-# ) -> User:
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = decode_jwt(
-#             token=token,
-#         )
-#         username: str = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#         token_data = TokenData(username=username)
-#     except InvalidTokenError:
-#         raise credentials_exception
-#     user = await get_user(
-#         session=session,
-#         token_data=token_data.username,
-#     )
-#     if user is None:
-#         raise credentials_exception
-#     return user
-#
-#
-# async def get_current_active_user(
-#     current_user: Annotated[User, Depends(get_current_user)],
-# ):
-#     if not current_user.active:
-#         raise HTTPException(status_code=400, detail="Inactive user")
-#     return current_user
